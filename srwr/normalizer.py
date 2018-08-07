43b310fbb3085604003f07e2aa8d9293972ba677
jinhongjung <jinhongjung@snu.ac.kr>
2018-08-07 19:07:18 +0900

    update iterator and normalizer

--- srwr/normalizer.py
+++ srwr/normalizer.py
@@ -9,7 +9,7 @@ def semi_row_normalize(A):
     d = abs(A).sum(axis=1)
 
     d = np.maximum(d, np.ones((n, 1)))
-    invd = 1.0/d
+    invd = 1.0 / d
     invd = np.reshape(invd, (1, -1))
     invD = spdiags(invd, 0, m, n)
     snA = invD * A
@@ -21,5 +21,10 @@ def semi_row_normalize(A):
 
     nAp = csr_matrix((abs(K[pos]), (I[pos], J[pos])), shape=(m, n))
     nAn = csr_matrix((abs(K[neg]), (I[neg], J[neg])), shape=(m, n))
+
+    print("nap")
+    print(nAp)
+    print("nan")
+    print(nAn)
 
     return nAp, nAn
